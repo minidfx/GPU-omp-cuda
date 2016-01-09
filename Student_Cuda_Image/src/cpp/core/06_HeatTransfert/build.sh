4262e8c0368dfa694553c320447c9c1143e85656
Burgy Benjamin <minidfx@gmail.com>
2016-01-09 14:49:47 +0100

    Add script for building remotely using Atom.

---
+++ Student_Cuda_Image/src/cpp/core/06_HeatTransfert/build.sh
@@ -0,0 +1,2 @@
+rsync -zrh --delete --progress . mse13@157.26.103.175:CUDA/toStudent/code/WCudaMSE/Student_Cuda_Image/src/cpp/core/06_HeatTransfert
+ssh mse13@157.26.103.175 "cd ~/CUDA/toStudent/code/WCudaMSE/Student_Cuda_Image;cbicc cuda"
