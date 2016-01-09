f9dee3fae351b264fea60b842584391975488fb4
Burgy Benjamin <minidfx@gmail.com>
2016-01-09 18:28:53 +0100

    Code cleanup.

--- Student_Cuda_Image/src/cpp/core/06_HeatTransfert/moo/device/HeatTransfertDeviceAdvanced.cu
+++ Student_Cuda_Image/src/cpp/core/06_HeatTransfert/moo/device/HeatTransfertDeviceAdvanced.cu
@@ -10,52 +10,56 @@ __global__ void crushAdvanced(float* ptrDevImageHeater, float* ptrDevImage, unsi
 __global__ void displayAdvanced(float* ptrDevImage, uchar4* ptrDevPixels, unsigned int size);
 
 __global__ void diffuseAdvanced(float* ptrDevImageInput, float* ptrDevImageOutput, unsigned int width, unsigned int height, float propagationSpeed)
+{
+  // Calucul threads available
+  const int NB_THREADS = Indice2D::nbThread();
+  const int TID = Indice2D::tid();
+
+  // Init service and variable required
+  HeatTransfertMathAdvanced math;
+  unsigned int totalPixels = width * height;
+  unsigned int s = TID;
+
+  while (s < totalPixels)
+  {
+    int i, j;
+    IndiceTools::toIJ(s, width, &i, &j);
+
+    if (i > 0 && i < (height - 1) && j > 0 && j < (width - 1))
     {
-    const int NB_THREADS = Indice2D::nbThread();
-    const int TID = Indice2D::tid();
+      float neighborsHeat[4];
+      neighborsHeat[0] = ptrDevImageInput[IndiceTools::toS(width, i - 1, j)];
+      neighborsHeat[1] = ptrDevImageInput[IndiceTools::toS(width, i + 1, j)];
+      neighborsHeat[2] = ptrDevImageInput[IndiceTools::toS(width, i, j - 1)];
+      neighborsHeat[3] = ptrDevImageInput[IndiceTools::toS(width, i, j + 1)];
 
-    HeatTransfertMathAdvanced math;
-    unsigned int wh = width * height;
-    unsigned int s = TID;
-    while (s < wh)
-	{
-	int i, j;
-	IndiceTools::toIJ(s, width, &i, &j);
-	if (i > 0 && i < (height - 1) && j > 0 && j < (width - 1))
-	    {
-
-	    float neighborsHeat[4];
-	    neighborsHeat[0] = ptrDevImageInput[IndiceTools::toS(width, i - 1, j)];
-	    neighborsHeat[1] = ptrDevImageInput[IndiceTools::toS(width, i + 1, j)];
-	    neighborsHeat[2] = ptrDevImageInput[IndiceTools::toS(width, i, j - 1)];
-	    neighborsHeat[3] = ptrDevImageInput[IndiceTools::toS(width, i, j + 1)];
-
-	    ptrDevImageOutput[s] = math.computeHeat(ptrDevImageInput[s], neighborsHeat, 4, propagationSpeed);
-	    }
-	else
-	    {
-	    ptrDevImageOutput[s] = ptrDevImageInput[s];
-	    }
-
-	s += NB_THREADS;
-	}
+      ptrDevImageOutput[s] = math.computeHeat(ptrDevImageInput[s], neighborsHeat, 4, propagationSpeed);
     }
+    else
+    {
+      ptrDevImageOutput[s] = ptrDevImageInput[s];
+    }
+
+    s += NB_THREADS;
+  }
+}
 
 __global__ void crushAdvanced(float* ptrDevImageHeater, float* ptrDevImage, unsigned int size)
-    {
-    const int NB_THREADS = Indice2D::nbThread();
-    const int TID = Indice2D::tid();
+{
+  const int NB_THREADS = Indice2D::nbThread();
+  const int TID = Indice2D::tid();
 
-    unsigned int s = TID;
-    while (s < size)
-	{
-	if (ptrDevImageHeater[s] > 0.0)
-	    {
-	    ptrDevImage[s] = ptrDevImageHeater[s];
-	    }
-	s += NB_THREADS;
-	}
+  unsigned int s = TID;
+  while (s < size)
+  {
+    if (ptrDevImageHeater[s] > 0.0)
+    {
+      ptrDevImage[s] = ptrDevImageHeater[s];
     }
+
+    s += NB_THREADS;
+  }
+}
 
 __global__ void displayAdvanced(float* ptrDevImage, uchar4* ptrDevPixels, unsigned int size)
     {
