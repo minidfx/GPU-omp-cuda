90c4eefdcdf31b5e669ed4171c91f34545137e70
Burgy Benjamin <minidfx@gmail.com>
2016-01-11 16:16:02 +0100

    Try to run the application only whether the previous build was succeeded.

--- Student_Cuda_Image/run.sh
+++ Student_Cuda_Image/run.sh
@@ -1,1 +1,9 @@
-ssh -C -X mse13@157.26.103.175 "cd ~/CUDA/toStudent/code/WCudaMSE/Student_Cuda_Image;gl cbicc cuda run"
+#!/bin/bash
+
+bash build.sh
+
+if [ $? != 0 ]; then
+  exit 1
+fi
+
+ssh -C mse13@157.26.103.175 "cd ~/CUDA/toStudent/code/WCudaMSE/Student_Cuda_Image;cbicc cuda run"
--- Student_OMP_Image/run.sh
+++ Student_OMP_Image/run.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
 bash ./build.sh $1
+
+if [ $? != 0 ]; then
+  exit 1
+fi
+
 ssh -C mse13@157.26.103.175 "cd ~/CUDA/toStudent/code/WCudaMSE/Student_OMP_Image;cbicc $1 run"
