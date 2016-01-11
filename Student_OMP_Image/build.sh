332267ddeb16dbcac071ef8d7faa5d2c852b5e49
Burgy Benjamin <minidfx@gmail.com>
2016-01-11 14:44:36 +0100

    Fix build script for OMP.

--- Student_OMP_Image/build.sh
+++ Student_OMP_Image/build.sh
@@ -1,2 +1,2 @@
-rsync -zrh --delete --exclude=*.json --exclude=*.sh --progress . mse13@157.26.103.175:CUDA/toStudent/code/WCudaMSE/Student_Cuda_Image
-ssh -C mse13@157.26.103.175 "cd ~/CUDA/toStudent/code/WCudaMSE/Student_Cuda_Image;cbicc $1"
+rsync -zrh --delete --exclude=*.json --exclude=*.sh --progress . mse13@157.26.103.175:CUDA/toStudent/code/WCudaMSE/Student_OMP_Image
+ssh -C mse13@157.26.103.175 "cd ~/CUDA/toStudent/code/WCudaMSE/Student_OMP_Image;cbicc $1"
--- Student_OMP_Image/run.sh
+++ Student_OMP_Image/run.sh
@@ -1,1 +1,1 @@
-ssh -C -X mse13@157.26.103.175 "cd ~/CUDA/toStudent/code/WCudaMSE/Student_Cuda_Image;gl cbicc cuda run"
+ssh -C -X mse13@157.26.103.175 "cd ~/CUDA/toStudent/code/WCudaMSE/Student_OMP_Image;gl cbicc gcc run"
