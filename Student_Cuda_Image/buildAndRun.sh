5c13bb9b08e7c845233fd8dad5030c47b9b10424
Burgy Benjamin <minidfx@gmail.com>
2016-01-23 09:10:18 +0100

    Improve compile scripts.

--- Student_Cuda_Image/buildAndRun.sh
+++ Student_Cuda_Image/buildAndRun.sh
@@ -3,7 +3,7 @@
 bash build.sh
 
 if [ $? != 0 ]; then
-  exit 1
+  exit $?
 fi
 
-ssh -C mse13@157.26.103.175 "cd ~/CUDA/toStudent/code/WCudaMSE/Student_Cuda_Image;cbicc cuda run"
+bash run.sh
--- Student_Cuda_Image/clean.sh
+++ Student_Cuda_Image/clean.sh
@@ -1,1 +1,3 @@
-ssh -C mse13@157.26.103.175 "cd ~/CUDA/toStudent/code/WCudaMSE/Student_Cuda_Image;gl cbicc cuda clean"
+#!/bin/bash
+
+ssh -C mse13@157.26.103.175 "cd ~/CUDA/toStudent/code/WCudaMSE/Student_Cuda_Image;cbicc cuda clean"
